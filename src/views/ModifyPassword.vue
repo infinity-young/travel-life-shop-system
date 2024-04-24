5a8d9be0d65b2d91d5b67d1c599e952bcd189fa7
yangshasha05 <yangshasha05@meituan.com>
2024-04-24 22:55:25 +0800

    密码修改页样式修改

--- src/views/ModifyPassword.vue
+++ src/views/ModifyPassword.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="login">
     <form>
-      <div class="form-group">
-        <label for="username">用户名</label>
-        <input type="text" id="username" v-model="username" required />
-      </div>
-      <div class="form-group">
-        <label for="oldpassword">旧密码</label>
-        <input type="oldpassword" id="oldpassword" v-model="oldpassword" required />
-      </div>
-      <div class="form-group">
-        <label for="newpassword">新密码</label>
-        <input type="newpassword" id="newpassword" v-model="newpassword" required />
-      </div>
-      <div class="form-group">
-        <label for="password">确认密码</label>
-        <input type="password" id="password" v-model="password" required />
-      </div>
-      <div class="form-group">
-        <label for="kaptcha">验证码</label>
-        <div class="kaptcha">
-          <img :src="kaptchaUrl" @click="refreshkaptcha" />
-        </div>
-        <input type="text" id="kaptcha" v-model="kaptcha" required />
-      </div>
+      <NecessaryInput
+        title="用户名"
+        :hasInputed="hasUsername"
+        v-model:inputValue="username"
+        autocomplete="username"
+      />
+      <NecessaryInput
+        title="旧密码"
+        :hasInputed="hasOldPassword"
+        v-model:inputValue="oldpassword"
+        autocomplete="oldpassword"
+      />
+      <NecessaryInput
+        title="新密码"
+        :hasInputed="hasNewPassword"
+        v-model:inputValue="newpassword"
+        autocomplete="newpassword"
+      />
+      <NecessaryInput
+        title="确认密码"
+        :hasInputed="hasPassword"
+        v-model:inputValue="password"
+        autocomplete="password"
+      />
+      <NecessaryInput
+        title="验证码"
+        :hasInputed="hasKaptcha"
+        :icon="kaptchaUrl"
+        v-model:inputValue="kaptcha"
+        @click-image="refreshkaptcha"
+      />
+
       <button type="submit" @click.prevent="submit" class="login-button">提交</button>
-      <button type="submit" @click.prevent="goBack" class="login-button">返回登录页</button>
+      <button type="submit" @click.prevent="goBack" class="login-button">返回</button>
     </form>
   </div>
 </template>
@@ -37,15 +46,25 @@ import store from '../stores/index'
 import { useToast } from 'vue-toastification'
 import router from '../router/index'
 import { KaptchaType } from '../../models/KaptchaType';
+import NecessaryInput from '../components/NecessaryInput.vue';
+
 
 export default {
+    components: {
+        NecessaryInput
+    },
     data () {
         return {
             username: '',
+            hasUsername: true,
             password: '',
+            hasPassword: true,
             newpassword: '',
+            hasNewPassword: true,
             oldpassword: '',
+            hasOldPassword: true,
             kaptcha: '',
+            hasKaptcha: true,
             kaptchaUrl: '',
             kaptchaCode: ''
         }
@@ -65,9 +84,9 @@ export default {
             router.push({ name: 'login' })
         },
         async submit () {
+            const toast = useToast()
             //如果新密码和确认密码不相等则给用户提示
             if (this.password !== this.newpassword) {
-                const toast = useToast()
                 toast.error('新密码和确认密码不同')
             } else if (this.kaptcha === this.kaptchaCode && this.password === this.newpassword) {
                 //将校验放到前端，验证码校验通过才请求登录接口
@@ -80,6 +99,9 @@ export default {
                 //密码修改成功以后返回登录页
                 if (data?.data?.success) {
                     this.logOut()
+                } else {
+                    //如果不成功提示原因
+                    toast.error(data?.data?.errMsg)
                 }
             }
         },
@@ -89,7 +111,8 @@ export default {
             this.kaptchaCode = kaptchaData.kaptchaCode
         },
         goBack () {
-            //返回登录页
+            //返回
+            this.$router.go(-1)
         }
     }
 }
@@ -110,24 +133,6 @@ form {
   flex-direction: column;
   align-items: center;
 }
-
-.form-group {
-  margin-bottom: 20px;
-}
-
-label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-input {
-  padding: 10px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  width: 300px;
-  font-size: 16px;
-}
-
 button {
   padding: 10px;
   border-radius: 5px;
@@ -142,16 +147,8 @@ button {
 button:hover {
   background-color: #0062cc;
 }
-.kaptcha {
-  display: inline-block;
-  margin-bottom: 10px;
-}
-
-.kaptcha img {
-  cursor: pointer;
-}
 
 .login-button {
-  margin-bottom: 200px;
+  margin-top: 20px;
 }
 </style>
