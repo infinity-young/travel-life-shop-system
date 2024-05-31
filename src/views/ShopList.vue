142c01528db7ecf0eb3fcfa7f685522c42b95e0d
yangshasha05 <yangshasha05@meituan.com>
2024-05-31 16:08:40 +0800

    商家列表页样式修改

--- src/views/ShopList.vue
+++ src/views/ShopList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="shop-info">
+    <div class="title-text">欢迎来到travel life 商家系统</div>
     <div class="header-container">
       <span>{{ title }}</span>
       <button @click="addShop">新增店铺</button>
@@ -18,9 +19,9 @@
         <tr v-for="(shop, index) in shopList" :key="index">
           <td>{{ shop.shopName }}</td>
           <td>{{ shop.advice }}</td>
-          <td @click="goToShopDetail(shop.shopId)">进入</td>
-          <td @click="goToProductManage(shop.shopId)">进入</td>
-          <td @click="goToCategoryManage(shop.shopId)">进入</td>
+          <td><button @click="goToShopDetail(shop.shopId)">进入</button></td>
+          <td><button @click="goToProductManage(shop.shopId)">进入</button></td>
+          <td><button @click="goToCategoryManage(shop.shopId)">进入</button></td>
         </tr>
       </tbody>
     </table>
@@ -47,61 +48,6 @@ export default defineComponent({
             user: {}
         }
     },
-    //此处栽了一个跟头，留着
-    // setup() {
-    //   console.log('===dd====' + store.getters['common/getToken'])
-
-    //   const shopList1 = getRequest(SHOP_LIST_PATH)
-    //   console.log('===shoplist======' + JSON.stringify(shopList1))
-    //   // const data = await getRequest(SHOP_LIST_PATH)
-
-    //   // console.log('======' + JSON.stringify(shopList))
-    //   // const data = ref(null)
-
-    //   // const fetchData = async () => {
-    //   //   const listData = await getRequest(SHOP_LIST_PATH)
-    //   //   data.value = listData
-    //   // }
-
-    //   // fetchData()
-
-    //   return {
-    //     // data
-    //     shopList1
-    //     // user: {},
-    //     // shopName: '',
-    //     // shopAddress: '',
-    //     // shopList: [
-    //     //   { name: '店铺1', address: '地址1' },
-    //     //   { name: '店铺2', address: '地址2' },
-    //     //   { name: '店铺3', address: '地址3' },
-    //     //   { name: '店铺4', address: '地址4' },
-    //     //   { name: '店铺5', address: '地址5' }
-    //     // ]
-    //   }
-    // },
-    // 正确的写法应该是这样
-    //   setup() {
-    //   const shopList = ref([]); // 使用 ref 创建一个响应式数组，ref从Vue中导出。
-
-    //   // 定义一个异步函数来获取数据
-    //   const fetchData = async () => {
-    //     try {
-    //       const response = await getRequest('/api/shop-list');
-    //       shopList.value = response.data; // 假设响应的数据在 data 字段中
-    //     } catch (error) {
-    //       console.error('请求失败:', error);
-    //     }
-    //   };
-
-    //   // 调用异步函数
-    //   fetchData();
-
-    //   // 返回响应式数据供模板使用
-    //   return {
-    //     shopList
-    //   };
-    // }
     mounted () {
         // 页面加载时，获取页面数据
         this.getListData()
@@ -143,35 +89,79 @@ export default defineComponent({
 })
 </script>
 <style scoped>
+.title-text {
+  font-size: 2em; /* 字体大小 */
+  text-align: center; /* 居中对齐 */
+  color: #c65577; /* 字体颜色 */
+  background: -webkit-linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%); /* 背景渐变 */
+  -webkit-background-clip: text; /* 文字背景裁剪 */
+  text-shadow: 0px 0px 6px rgba(251, 221, 221, 0.901); /* 文字阴影 */
+  font-weight: bold; /* 字体加粗 */
+  margin: 20px;
+  animation: glow 9s ease-in-out infinite alternate; /* 动画效果 */
+}
+
+/* 动画关键帧 */
+@keyframes glow {
+  from {
+    text-shadow: 0 0 10px #eba6a6, 0 0 20px #e57a7a, 0 0 30px #be4a5a, 0 0 40px #e65050,
+      0 0 50px #e83547, 0 0 60px #e54242, 0 0 70px #e60000;
+  }
+  to {
+    text-shadow: 0 0 10px #eba6a6, 0 0 20px #e57a7a, 0 0 30px #be4a5a, 0 0 40px #e65050,
+      0 0 50px #e83547, 0 0 60px #e54242, 0 0 70px #e60000;
+  }
+}
 .shop-info {
-  max-width: 600px;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   margin: 0 auto;
   padding: 20px;
   font-family: Arial, sans-serif;
 }
 .header-container {
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   margin-bottom: 10px;
 }
 
 table {
-  border-collapse: collapse;
   width: 100%;
+  text-align: center;
+  background-color: rgb(215, 218, 221);
+  border-radius: 12px;
+  border-spacing: 0; /* 用于替代 border-collapse: collapse; */
+  overflow: hidden; /* 确保圆角被显示 */
 }
 
 th,
 td {
   padding: 10px;
-  text-align: left;
   border: 1px solid #ccc;
 }
 
 th {
-  background-color: #9d2323;
+  background-color: #aeadad;
 }
+
+/* 针对表格最后一行的第一个和最后一个单元格设置圆角 */
+tr:last-child td:first-child {
+  border-bottom-left-radius: 12px;
+}
+
+tr:last-child td:last-child {
+  border-bottom-right-radius: 12px;
+}
+
 .footer-container {
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
